d60708052ff2e0f8401b91dcac3c701384741c55
Nur Alam Hasabie <39056147+AlamHasabie@users.noreply.github.com>
2024-12-20 14:14:24 +0700

    Update test_write_parquet_local.py
    Use current directory instead of data

--- test_write_parquet_local.py
+++ test_write_parquet_local.py
@@ -2,7 +2,7 @@ from datachain import DataChain
 import os
 
 LOCAL_DATASET_URI = os.path.join("file://", os.getcwd(), "sample_dataset/")
-PARQUET_PATH = os.path.join("file://", os.getcwd(), "data", "dataset.parquet")
+PARQUET_PATH = os.path.join("file://", os.getcwd(), "dataset.parquet")
 dataset_chain = DataChain.from_storage(
     LOCAL_DATASET_URI,
     update=True
