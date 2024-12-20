0c0530b04f6a56123a38103cde84ec4bd01ce079
Nur Alam Hasabie <39056147+AlamHasabie@users.noreply.github.com>
2024-12-20 14:13:59 +0700

    Update test_load_parquet_local.py
    Use current directory instead of ./data

--- test_load_parquet_local.py
+++ test_load_parquet_local.py
@@ -1,6 +1,6 @@
 from datachain import C, DataChain
 import os
 
-PARQUET_PATH = os.path.join("file://", os.getcwd(), "data", "dataset.parquet")
+PARQUET_PATH = os.path.join("file://", os.getcwd(), "dataset.parquet")
 dataset = DataChain.from_parquet(PARQUET_PATH)
 dataset.show()
